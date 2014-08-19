f6abda81d98e06872605784209013deb66639bc3
Eddy Ernesto del Valle Pino <eddy@merchise.org>
2014-08-19 14:30:52 -0400

    Jump to 1.2.11.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-version = '1.2.10'
+version = '1.2.11'
 
 setup(
     name='spine',
