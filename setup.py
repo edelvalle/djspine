78d9bd02a67bd98b0ae5ca15e422a0b34e865c4f
Eddy Ernesto del Valle Pino <eddy@merchise.org>
2014-09-06 18:46:43 -0400

    Update xoyuz dependency version.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     install_requires=[
         'Django>=1.5,<1.8',
         'xoutil>=1.6,<1.7',
-        'xoyuz>=0.2.5,<0.3',
+        'xoyuz>=0.2.7,<0.3',
         'python-dateutil>=2.1,<3'
     ],
 )
